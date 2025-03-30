c215d9e9489469ef954179b08a726433e1a1d730
begugithub <166634838+begugithub@users.noreply.github.com>
2025-03-30 16:18:57 +0700

    Update oh.sh

--- oh.sh
+++ oh.sh
@@ -3,6 +3,8 @@ mkdir OtohitsApp
 cd OtohitsApp
 echo "/login:386f7519-f1df-45f6-939e-613b049a8b9c" > otohits.ini
 echo "/autoupdate" >> otohits.ini
+echo "/nosandbox" >> otohits.ini
+
 tar -xzf ../OtohitsApp_5068_linux_portable.tar.gz
 
 ./otohits-app
